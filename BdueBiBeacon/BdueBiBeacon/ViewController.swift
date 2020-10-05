e15fbee840c10da82274008e2cfe142434b3d7a7
Bhavik Patel <bhavik.patel@iottive.com>
2020-10-05 11:24:48 +0530

    initial project commit

---
+++ BdueBiBeacon/BdueBiBeacon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BdueBiBeacon
+//
+//  Created by Bhavik Patel on 05/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
